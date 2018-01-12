947a106add7a3b98a31a1c8cef4714ca38e9ce9f
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-01-12 14:53:39 -0500

    First commit

---
+++ makeNegative.js
@@ -0,0 +1,23 @@
+// Return Negative
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+// Example:
+// makeNegative(1); // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0); // return 0
+// Notes:
+// The number can be negative already, in which case no change is required.
+// Zero (0) can't be negative, see examples above.
+
+function makeNegative(num) {
+    // Code?
+    if(num > 0 ){
+    return num*-1;
+    }
+    else if(num < 0){
+      return num*1;
+    }
+    else {
+    return 0;
+    }
+  }
